2c951bdabeadf8e7d3a91d1155621507f00843b6
PhotonicsOpenProjects <108125500+PhotonicsOpenProjects@users.noreply.github.com>
2023-01-18 09:42:33 +0100

    Update ONEPIX_acquisition.py
    Correction of the acquisition duration

--- ONE-PIX_soft/ONEPIX_acquisition.py
+++ ONE-PIX_soft/ONEPIX_acquisition.py
@@ -12,7 +12,7 @@ ONEPIX data initialisation
 json_path="./acquisition_param_ONEPIX.json"
 acquisition_config = OPConfig(json_path)
 OP_init(acquisition_config)
-print(f"Estimated acquisition duration : {acquisition_config.pattern_lib.nb_patterns*acquisition_config.periode_pattern/(60*1000)} min ")
+print(f"Estimated acquisition duration : {round(1.5*acquisition_config.pattern_lib.nb_patterns*acquisition_config.periode_pattern/(60*1000),2)} min ")
 """
 Start ONEPIX acquisition  
 """
