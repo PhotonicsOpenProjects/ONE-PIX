c632e63305ae87a4eadd0fefae917d09e7ba0471
garussias <grussias@photonics-open-projects.com>
2024-02-07 10:20:39 +0100

    add color for echo in install.sh

--- install/install.sh
+++ install/install.sh
@@ -1,8 +1,11 @@
-echo 'python dependencies installation with pip'
 
+echo -e '\e[32mpython dependencies installation with pip'
+
+echo -e '\033[0m_________________________________________________'
 pip install -r requirements.txt
 
-echo 'opencv python dependencies insatllation with apt'
+echo -e '\e[32m opencv python dependencies insatllation with apt'
+echo -e '\033[0m_________________________________________________'
 
 sudo apt-get -y install build-essential cmake pkg-config
 sudo apt -y install libjpeg-dev libtiff5-dev libjasper-dev libpng-dev
@@ -16,7 +19,8 @@ sudo apt -y install libhdf5-dev libhdf5-serial-dev libhdf5-103
 sudo apt -y install libqt5gui5 libqt5webkit5 libqt5test5 python3-pyqt5
 sudo apt -y install python3-dev
 
-echo 'cut and paste a new config.txt  in boot '
+echo -e '\e[32mcut and paste a new config.txt  in boot '
+echo -e '\033[0m_________________________________________________'
 
 sudo rm /boot/config.txt
 sudo cp ~/Desktop/ONE-PIX/install/config.txt /boot
