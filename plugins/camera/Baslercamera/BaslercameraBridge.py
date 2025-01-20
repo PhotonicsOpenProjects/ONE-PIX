73f77994528b6aec355101d545a303d77d409855
Lisa UGUEN <luguen@Perfos.local>
2025-01-20 15:45:39 +0100

    début d'implémentation caméra basler

--- plugins/camera/Baslercamera/BaslercameraBridge.py
+++ plugins/camera/Baslercamera/BaslercameraBridge.py
@@ -3,28 +3,27 @@ import os
 from datetime import date
 import time
 
-class BaslerCameraBridge:
+class BaslercameraBridge:
 
     def __init__(self):
         pass
 
     def init_camera(self):
-        try:
-            # Création d'une instance de caméra
-            self.camera = pylon.InstantCamera(pylon.TlFactory.GetInstance().CreateFirstDevice())
-            self.camera.Open()
-            
-            # Configuration par défaut de la caméra
-            self.camera.Width.Value = min(1024, self.camera.Width.Max)
-            self.camera.Height.Value = min(768, self.camera.Height.Max)
-            self.camera.OffsetX.Value = 0
-            self.camera.OffsetY.Value = 0
-            self.camera.ExposureTime.SetValue(20000.0)  # Exemple d'exposition en microsecondes
-            self.camera.Gain.SetValue(0.0)             # Gain minimal
-            
-            print("Pylon camera is connected and initialized.")
-        except Exception as e:
-            print(f"Error initializing Pylon camera: {e}")
+        # Création d'une instance de caméra
+        self.camera = pylon.InstantCamera(pylon.TlFactory.GetInstance().CreateFirstDevice())
+        self.camera.Open()
+        
+        # Configuration par défaut de la caméra
+        self.camera.Width.Value = min(1024, self.camera.Width.Max)
+        self.camera.Height.Value = min(768, self.camera.Height.Max)
+        self.camera.OffsetX.Value = 0
+        self.camera.OffsetY.Value = 0
+        self.camera.ExposureTime.SetValue(20000.0)  # Exemple d'exposition en microsecondes
+        self.camera.Gain.SetValue(0.0)             # Gain minimal
+        
+        print("Pylon camera is connected and initialized.")
+
+
 
     def image_capture(self, tag, save_path):
         try:
@@ -66,3 +65,6 @@ class BaslerCameraBridge:
             print("Pylon camera is disconnected.")
         except Exception as e:
             print(f"Error closing Pylon camera: {e}")
+            
+            
+    
