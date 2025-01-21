0cec65721a8dab3897090ff62a9bc7d7a3c12f78
garussias <grussias@photonics-open-project.com>
2025-01-21 14:37:35 +0100

    add 3d view in eaxampel_profilo_analysis

--- app/python_code_examples/example_profilo_analysis.py
+++ app/python_code_examples/example_profilo_analysis.py
@@ -12,10 +12,13 @@ sys.path.append(f"..{os.sep}..")
 import numpy as np
 import matplotlib.pyplot as plt
 from plugins.imaging_methods.Profilo.ImageReconstruction import ProfiloReconstruction
+from plugins.imaging_methods.Profilo.ImageAnalysis import Analysis
 
 rec=ProfiloReconstruction(r"C:\Users\grussias\Desktop\repo git\Photonics_bretagne\profilo.npy")
 depth_map=rec.reconstruction()
+an=Analysis()
+an.plot_reconstructed_image(depth_map)
 
-plt.figure()
-plt.imshow(depth_map)
-plt.show()
+# plt.figure()
+# plt.imshow(depth_map)
+# plt.show()
