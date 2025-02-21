8cd48a046d7b0d6ef4a4ebf36557048069c3d772
garussias <grussias@photonics-open-project.com>
2025-02-21 15:13:12 +0100

    update profilo reconstruction to change normalisation method
    update two diffrents normalisation  : with background fiiting plan and with measured reference

--- app/python_code_examples/example_profilo_analysis.py
+++ app/python_code_examples/example_profilo_analysis.py
@@ -2,7 +2,7 @@
 """
 Created on Tue Jan 21 10:15:25 2025
 
-@author: luguen
+@authors: luguen grussias
 """
 
 import os
@@ -13,9 +13,17 @@ import numpy as np
 import matplotlib.pyplot as plt
 from plugins.imaging_methods.Profilo.ImageReconstruction import ProfiloReconstruction
 from plugins.imaging_methods.Profilo.ImageAnalysis import Analysis
+from core.hardware.coregistration_lib import *
 
-rec=ProfiloReconstruction(r"S:\Alternants\En-Cours\2022 Lisa Uguen\microscope hyperspectral\Topographie\objet\profilo.npy")
-hauteur=rec.reconstruction()
+
+rec=ProfiloReconstruction("put path of the raw measure of the sample here")
+rec.load_reference("put path of the raw measure of the reference here")
+
+depth_map_mes_ref_norm=rec.reconstruction_with_ref()
+depth_map_plan_ref=rec.reconstruction()
+
+
+
 an=Analysis()
-an.plot_reconstructed_image(hauteur)
-
+an.plot_reconstructed_image(depth_map_plan_ref)
+an.plot_reconstructed_image(depth_map_mes_ref_norm)
