cf509e337a9ced0b3a9b34a72223a43f2ab5007c
garussias <grussias@photonics-open-project.com>
2025-09-29 13:57:15 +0200

    update HAS reconsctruction to new refacto

--- app/python_code_examples/example_acquisiton.py
+++ app/python_code_examples/example_acquisiton.py
@@ -1,6 +1,6 @@
 from onepix.Acquisition import Acquisition
 
-acq = Acquisition(imaging_method_name="HadamardSplit")
+acq = Acquisition(imaging_method_name="Addressing")
 acq.thread_acquisition()
 acq.save_raw_data()
 
--- app/python_code_examples/example_acquisiton_and_recosntruction.py
+++ app/python_code_examples/example_acquisiton_and_recosntruction.py
@@ -2,12 +2,18 @@ from onepix.Acquisition import Acquisition
 from onepix.Reconstruction import Reconstruction
 import numpy as np 
 import matplotlib.pyplot as plt 
-# acq = Acquisition(imaging_method_name="FourierSplit")
-# acq.thread_acquisition()
-# acq.save_raw_data()
-# print(list(acq.acquisition_results.keys()))
+
+
+acq = Acquisition(imaging_method_name="Addressing")
+acq.thread_acquisition()
+acq.save_raw_data()
+
 rec=Reconstruction(plot_result=True)
 rec.data_reconstruction()
+print(list(rec.reconstruction_results.keys()))
+plt.figure()
+plt.imshow(rec.reconstruction_results["result2plot"])
+plt.show()
 rec.save_reconstructed_image()
 
 
